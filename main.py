6ce1c1303bac3c577db3db27b7fd7dc6c7d7d1ac
jennylee1996 <lee19960807@gmail.com>
2024-04-29 17:26:11 +0800

    Initital commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def train_model():
+    # Use a breakpoint in the code line below to debug your script.
+    print('model start')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    train_model()
